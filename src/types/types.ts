400bdec8b00b320996eb11843085581659d77812
heitxrr <161774119+heitxrr@users.noreply.github.com>
2025-01-12 23:17:07 -0300

    Ciando arquivo type, e adicionando a primeira tipagem

---
+++ src/types/types.ts
@@ -0,0 +1,13 @@
+import { StaticImageData } from "next/image";
+
+// export type StandardType = {
+//     id: string;
+//     name: string;
+//     description?: string;
+//     createdAt: Date;
+//     updatedAt: Date;
+// };
+
+export type RodapeProps = {
+    img: StaticImageData;
+}
