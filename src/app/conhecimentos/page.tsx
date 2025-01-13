249dad1f3877ea2de0c7a892795f4cb76e1aee3f
heitxrr <heitorrrocha04@outlok.com>
2025-01-12 22:50:12 -0300

    Finalizando criação de paginas

---
+++ src/app/conhecimentos/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HabilidadePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Habilidades</h1>
+            <p>Bem-vindo à página Habilidade. Aqui você encontrará informações sobre minhas habilidades.</p>
+        </div>
+    );
+};
+
+export default HabilidadePage;
